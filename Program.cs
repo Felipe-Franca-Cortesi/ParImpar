7d4109dcbf4fc8fa97bb9f745fe5f4ac16e4c933
Felipe França <contatofelipefranca12@gmail.com>
2020-10-16 20:39:07 -0300

    Adiciona a conclusão do exercício ParImpar

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace parImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroParImpar, restoDivisao;
+            string textoNumeroParImpar;
+            bool numeroParImparValido;
+
+            Console.Clear();
+            Console.WriteLine("--- Número par ou impar");
+
+            Console.Write("Digite um número: ");
+            textoNumeroParImpar = Console.ReadLine();
+
+            numeroParImparValido = Int32.TryParse(textoNumeroParImpar, out numeroParImpar);
+            restoDivisao = numeroParImpar % 2;
+            
+            if (!numeroParImparValido)
+            {
+                Console.WriteLine("Valor inválido");
+                Environment.Exit(-1);
+            }
+            else if (restoDivisao > 0)
+            {
+                Console.WriteLine($"{numeroParImpar} é impar");
+            }
+            else
+            {
+                Console.WriteLine($"{numeroParImpar} é par");
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ParImpar
-[console: ParImpar] Receba um número. Exiba "par" ou "ímpar", conforme o caso.
+*[console: ParImpar] Receba um número. Exiba "par" ou "ímpar", conforme o caso.*
+
+**Dica: um número é ímpar caso possua resto ao ser dividido por 2.**
+
+`Exemplo:`
+
+```
+Digite um número: 17
+17 é ímpar
+```
